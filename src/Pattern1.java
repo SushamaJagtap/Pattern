675e45d711ab4090b701fdef3cdbf7bb5f7f30e5
SushamaJagtap <sushamajagtap3843@gmail.com>
2019-07-09 12:04:26 +0530

    First commit through git bash

---
+++ src/Pattern1.java
@@ -0,0 +1,127 @@
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		
+		/*for(int i=1;i<=4;i++)
+		{
+			for(int j=1;j<=4;j++)
+			{
+				System.out.print("*");
+			}
+		       System.out.println();
+		}*/
+		
+		/*for(int i=1;i<=4;i++)
+		{
+			for(int j=4;j>=i;j--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}*/
+		
+		/*for(int i=1;i<=4;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}*/
+		
+//------------------------assi--------------------------------------------
+		/*int cnt=0;
+		for(int i=0;i<=4;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+			   cnt++;	
+			   System.out.print(+cnt);
+			}
+			System.out.println();
+		}*/
+		
+		/*for(int i=1;i<=4;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.printf(" %c ", j+64);
+			}
+			
+			System.out.println();
+		}*/
+		
+		//int cnt=63;
+		/*for(int i=1;i<=4;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+		
+				System.out.printf(" %c ", i+64);
+			}
+			System.out.println();
+		}*/
+		
+		/*for(int i=1;i<=4;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+		}*/
+		
+		/*for(int i=1;i<=4;i++)
+		{
+			for(int j=4;j>=i;j--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=4;j>=i;j--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}*/
+		
+		/*for(int i=1;i<=4;i++)
+		{
+			
+			for(int j=3;j>=i;j--)
+			{
+				System.out.print(" ");
+			}
+				for(int k=1;k<=i;k++)
+				{
+					
+					System.out.print("*");
+				}
+				System.out.println();
+			}*/
+		
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=4;j>=i;j--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+			for(int k=1;k<=i;k++)
+			{
+				System.out.print(" ");
+			}
+		}
+		}
+			
+	
+		
+		
+		
+	}
+
+
